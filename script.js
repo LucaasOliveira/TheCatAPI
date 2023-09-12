8d23742d103887cb5f8f7553e6fa22884ce83591
LucaasOliveira <olucasoliveira013@gmail.com>
2023-09-12 20:18:05 -0300

    feat: criar funcao carregar informacoes das racas

--- script.js
+++ script.js
@@ -27,12 +27,31 @@ breedSelect.addEventListener("change", () => {
     }
 });
 
-const card = document.querySelector(".card");
+async function loadBreeds() {
+    const breedsUrl = "https://api.thecatapi.com/v1/breeds";
+
+    try {
+        const response = await axios.get(breedsUrl);
+        const breedsData = response.data;
 
 
+        const defaultOption = document.createElement("option");
+        defaultOption.value = "";
+        defaultOption.text = "Escolha uma raça";
+        breedSelect.appendChild(defaultOption);
 
+        breedsData.forEach((breed) => {
+            const option = document.createElement("option");
+            option.value = breed.id;
+            option.text = breed.name;
+            breedSelect.appendChild(option);
+        });
+    } catch (error) {
+        console.error("Erro ao carregar as raças:", error);
+    }
+}
 
-
+loadBreeds();
 
 
 
