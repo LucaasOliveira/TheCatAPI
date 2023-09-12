d6a16d81d043bcf2ed80467519249887532183bb
LucaasOliveira <olucasoliveira013@gmail.com>
2023-09-12 20:17:08 -0300

    feat: capurar o evento para filtrar as imagens

--- script.js
+++ script.js
@@ -18,14 +18,7 @@ async function fetchCatImagesByBreed(breedId) {
     }
 }
 
-axios.get("https://api.thecatapi.com/v1/images/search?api_key=live_SZgisQ7fCM4pD5We0Bbyj0CdX58Hy0mtGoMwMNrxP8uUM3VtRDpxd8XjRjvEKydZ")
-    .then((response) => {
-        //manipular reposta
-        console.log(response.data)
-    })
-    .catch((error) => {
-        //manipular erro
-    })
+const breedSelect = document.querySelector("select");
 
 breedSelect.addEventListener("change", () => {
     const selectedBreedId = breedSelect.value;
