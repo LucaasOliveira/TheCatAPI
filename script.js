ddf300016504c63a4fbf43c8c36da7ada8e17e5a
LucaasOliveira <olucasoliveira013@gmail.com>
2023-09-12 20:14:45 -0300

    feat: criar funcao buscar imagens por raca

--- script.js
+++ script.js
@@ -1,3 +1,22 @@
+async function fetchCatImagesByBreed(breedId) {
+    const apiUrl = `https://api.thecatapi.com/v1/images/search?limit=10&breed_ids=${breedId}&api_key=live_SZgisQ7fCM4pD5We0Bbyj0CdX58Hy0mtGoMwMNrxP8uUM3VtRDpxd8XjRjvEKydZ`;
+
+    try {
+        const response = await axios.get(apiUrl);
+        const data = response.data;
+
+        const cards = document.querySelector(".cards");
+        cards.innerHTML = "";
+
+        data.forEach((cat) => {
+            const catImage = document.createElement("img");
+            catImage.src = cat.url;
+            cards.appendChild(catImage);
+        });
+    } catch (error) {
+        console.error("Erro ao acessar a API:", error);
+    }
+}
 
 axios.get("https://api.thecatapi.com/v1/images/search?api_key=live_SZgisQ7fCM4pD5We0Bbyj0CdX58Hy0mtGoMwMNrxP8uUM3VtRDpxd8XjRjvEKydZ")
     .then((response) => {
